8070443db33be6b3da13391bfee6d3daa4876341
Serhii Kupriienko <skupriienko@anaconda.com>
2023-02-03 10:39:58 +0200

    Update run_test.py, add folium to geopandas run section

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,5 +1,16 @@
 import os
 import geopandas
 
-fname = os.path.join(os.environ['RECIPE_DIR'], 'test_data', 'test.shp')
-geopandas.read_file(fname)
+
+fname_shp = os.path.join(os.environ['RECIPE_DIR'], 'test_data', 'test.shp')
+geopandas.read_file(fname_shp)
+
+fname_cpg = os.path.join(os.environ['RECIPE_DIR'], 'test_data', 'test.cpg')
+geopandas.read_file(fname_cpg)
+
+fname_dbf = os.path.join(os.environ['RECIPE_DIR'], 'test_data', 'test.dbf')
+geopandas.read_file(fname_dbf)
+
+fname_shx = os.path.join(os.environ['RECIPE_DIR'], 'test_data', 'test.shx')
+geopandas.read_file(fname_shx)
+
